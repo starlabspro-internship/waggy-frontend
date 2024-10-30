d7b3073d983cb07a08cb0246da19a84e8e8ee3f7
lavdrimloku <lavdrimloku@yahoo.com>
2024-10-30 11:30:38 +0100

    login, signup and small details

---
+++ assets/js/signup.js
@@ -0,0 +1,12 @@
+// Toggle Password Visibility
+function togglePassword(fieldId) {
+    const passwordField = document.getElementById(fieldId);
+    passwordField.type = passwordField.type === 'password' ? 'text' : 'password';
+}
+
+// Form Validation
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add your validation logic here
+    alert('Form submitted successfully!');
+});
