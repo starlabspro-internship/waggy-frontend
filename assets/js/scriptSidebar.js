0f65549436022db8088f0cce355037da1f7df4e3
AliDreshaj12 <151794718+AliDreshaj12@users.noreply.github.com>
2024-10-30 15:05:08 +0100

    sidebar

---
+++ assets/js/scriptSidebar.js
@@ -0,0 +1,29 @@
+const sidebar=document.querySelector(".sidebar");
+const sidebarToggler =document.querySelector(".sidebar-toggler");
+const menuToggler = document.querySelector(".menu-toggler");
+
+const collapsedSidebarHeight="56px";
+const fullSidebarHeight ="calc(100vh - 32px)";
+
+sidebarToggler .addEventListener("click",() =>{
+    sidebar.classList.toggle("collapsed");
+});
+
+const toggleMenu = (isMenuActive) =>{
+    sidebar.style.height=isMenuActive ? `${sidebar.scrollHeight}px` :collapsedSidebarHeight;
+    menuToggler.querySelector("span").innerText = isMenuActive ? "close" : "menu";
+}
+menuToggler.addEventListener("click", ()=>{
+    toggleMenu(sidebar.classList.toggle("menu-active"));
+});
+
+window.addEventListener("resize",() =>{
+    if(window.innerWidth >=1024){
+        sidebar.style.height=fullSidebarHeight;
+    }else{
+        sidebar.classList.remove("collapsed");
+        sidebar.style.height="auto";
+        toggleMenu(sidebar.classList.contains("menu-active"));
+    }
+});
+
